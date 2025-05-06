e76df5323dc5093530492439654b608f899845a5
Beknur <106554639+BEknUur@users.noreply.github.com>
2025-05-06 18:28:32 +0500

    prod

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .database.session import engine, Base
 from .routes import cultures, quiz,  chat,media
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../.env"))
 
 app = FastAPI(
     title="Culturology API",
